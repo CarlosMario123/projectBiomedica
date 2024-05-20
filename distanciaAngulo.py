c35826afdf48f3bc82eae0f7a33e550228f51668
DiegoG0477 <diego.14.13.lopez@gmail.com>
2024-05-19 21:27:04 -0600

    update

--- distanciaAngulo.py
+++ distanciaAngulo.py
@@ -1,9 +1,9 @@
-from mpu6050 import MPU6050
+from mpu6050 import mpu6050
 import time
 import RPi.GPIO as GPIO
 
 # Inicialización del objeto mpu para el sensor giroscopio
-mpu = MPU6050(0x68)
+mpu = mpu6050(0x68)
 
 # Configuración inicial del sensor de distancia
 GPIO.setmode(GPIO.BCM)
@@ -64,16 +64,3 @@ while True:
 
     # Esperar un minuto antes de la próxima lectura
     time.sleep(60)
-
-
-# 1. Importa las bibliotecas necesarias: mpu6050 para el sensor MPU6050, time para las funciones de tiempo, y RPi.GPIO para interactuar con los pines GPIO de la Raspberry Pi.
-
-# 2. Inicializa el sensor MPU6050 en la dirección I2C 0x68.
-
-# 3. Configura los pines GPIO para el sensor de distancia. El pin TRIG se configura como salida y el pin ECHO como entrada.
-
-# 4. Define la función leer_giroscopio(), que lee los datos del acelerómetro del MPU6050 y calcula el ángulo basado en los valores de los ejes X y Z. El ángulo calculado depende del cuadrante en el que se encuentren los valores de X y Z.
-
-# 5. Define la función leer_distancia(), que utiliza los pines TRIG y ECHO para medir la distancia. Envía un pulso corto en el pin TRIG y luego mide el tiempo que tarda en recibir el eco en el pin ECHO. Este tiempo se convierte en una distancia en centímetros.
-
-# 6. Entra en un bucle infinito donde lee y calcula los valores del giroscopio y la distancia, los imprime y luego espera un minuto antes de repetir el proceso.
