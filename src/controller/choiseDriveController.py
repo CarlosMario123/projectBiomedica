7edb6fd04428edb1c7ff08d7cf89591be718c1d3
itsWill32 <williamytjas@gmail.com>
2024-06-07 07:36:26 -0600

    frame recorrido completed

--- src/controller/choiseDriveController.py
+++ src/controller/choiseDriveController.py
@@ -21,9 +21,11 @@ class ChoiseDriverController:
         choferes = self.obtener_choferes()
         chofer_id = next((id_chofer for id_chofer, name in choferes if name == chofer_name), None)
 
-        # Almacenar el ID del chofer en ContextChofer
-        ContextChofer.get_instance().set_chofer_id(chofer_id)
-        print(f"Chofer seleccionado: {ContextChofer.get_instance().get_chofer_id()}")
+        # Almacenar el ID y el nombre del chofer en ContextChofer
+        context_chofer = ContextChofer.get_instance()
+        context_chofer.set_chofer_id(chofer_id)
+        context_chofer.set_name_chofer(chofer_name)
+        print(f"Chofer seleccionado: {context_chofer.get_chofer_id()}")
 
         # Cambiar a la vista de recorrido
         self.root.cambiarVista("recorrido")
