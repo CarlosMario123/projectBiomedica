0582fb79529f32b01ac641509adb259188515ba3
itsWill32 <williamytjas@gmail.com>
2024-06-07 06:26:32 -0600

    update

--- src/view/frames/inicio.py
+++ src/view/frames/inicio.py
@@ -3,7 +3,7 @@ from PIL import Image, ImageTk
 
 class InicioView(tk.Frame):
     def __init__(self, master, controller):
-        super().__init__(master)
+        super().__init__(master, bg="#F6F5FB")
         self.controller = controller
         self.init_ui()
         self.after(1000, self.btnAnimate)  # evento para prolongar eventos a un tiempo especifico
@@ -12,11 +12,8 @@ class InicioView(tk.Frame):
       
         self.addBackgroudImage()
         self.addLogos()
-        self.label1 = tk.Label(self, text="Maneje con preocupación y sin distracciones", font=("", 18, "bold"), width=40, height=5)
-        self.label1.place(x=300, y=1)
-
-        self.label2 = tk.Label(self, text="¡Feliz viaje!", font=("", 16, "bold"))
-        self.label2.place(x=550, y=100)
+        self.label1 = tk.Label(self, text="Monitoreo de Postura para Conducción de Vehículos de Transporte Público de Ruta Prolongada", font=("", 11, "bold"), bg="#F6F5FB", fg="black")
+        self.label1.place(x=525, y=10)
      
 
     def btnAnimate(self):
@@ -34,28 +31,36 @@ class InicioView(tk.Frame):
 
     def addBackgroudImage(self):
         self.img = Image.open("img/fondo.png")
-        self.img = self.img.resize((400, 300), Image.LANCZOS)
+        self.img = self.img.resize((490, 380), Image.LANCZOS)
         self.imgtk = ImageTk.PhotoImage(self.img)
 
         # Crear un Label con la imagen y centrarla
-        label_img = tk.Label(self, image=self.imgtk)
-        label_img.place(x=-250, y=-220, relwidth=1, relheight=1)
-        
-    def addLogos(self):
-        self.img1 = Image.open("img/rs.jpeg")
-        self.img1 = self.img1.resize((70, 70), Image.LANCZOS)
-        self.imgtk1 = ImageTk.PhotoImage(self.img1)
+        label_img = tk.Label(self, image=self.imgtk, bg="#F6F5FB")
+        label_img.place(x=-1, y=-1)
 
-        self.img2 = Image.open("img/ado.jpeg")
-        self.img2 = self.img2.resize((70, 70), Image.LANCZOS)
+        self.img2 = Image.open("img/project91.png")
+        self.img2 = self.img2.resize((750, 210), Image.LANCZOS)
         self.imgtk2 = ImageTk.PhotoImage(self.img2)
 
+        # Crear un Label con la imagen y centrarla
+        label_img2 = tk.Label(self, image=self.imgtk2)
+        label_img2.place(x=500, y=50)
+        
+    def addLogos(self):
+        self.logo1 = Image.open("img/rs.jpeg")
+        self.logo1 = self.logo1.resize((70, 70), Image.LANCZOS)
+        self.logotk1 = ImageTk.PhotoImage(self.logo1)
+
+        self.logo2 = Image.open("img/ado.jpeg")
+        self.logo2 = self.logo2.resize((70, 70), Image.LANCZOS)
+        self.logotk2 = ImageTk.PhotoImage(self.logo2)
+
         # Crear Labels para los logos
-        label_logo1 = tk.Label(self, image=self.imgtk1)
-        label_logo2 = tk.Label(self, image=self.imgtk2)
+        label_logo1 = tk.Label(self, image=self.logotk1)
+        label_logo2 = tk.Label(self, image=self.logotk2)
 
         # Obtener el tamaño de la ventana y las imágenes
-        window_height = 600  # Altura de la ventana
+        window_height = 720  # Altura de la ventana
         logo_height = 80   # Altura de los logotipos
 
         # Calcular la posición y colocar los logos en la parte inferior izquierda
