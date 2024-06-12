223233beee24bc4806443f682bf30dbc9c3277f2
itsWill32 <williamytjas@gmail.com>
2024-06-11 20:17:34 -0600

    build

--- cronlaunch.sh
+++ cronlaunch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "La aplicación no está en ejecución. Iniciando..."
 # Activar el entorno virtual de Python
-source /home/pi/Desktop/projectBiomedica/venv/bin/activate
+source /home/raspberry/Documentos/projectBiomedica/venv/bin/activate
 # Esperar unos segundos antes de iniciar la aplicación
 #sleep 5
 # Iniciar la aplicación en segundo plano y registrar la salida
-python /home/pi/Desktop/projectBiomedica/app.py
+python3 /home/raspberry/Documentos/projectBiomedica/app.py
 #fi
